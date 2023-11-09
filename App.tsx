c8068be118e200c9cc013fe3057057365e9b34a8
Uzair Aslam <uzairaslam990@gmail.com>
2023-11-09 06:55:04 -0800

    practice helloworld

---
+++ App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, { Component } from 'react'
+import { ScrollView, Text, View } from 'react-native'
+
+export default class App extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text>pakistan zindabad </Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, saepe aspernatur suscipit in ipsa enim id vel cupiditate recusandae iure at facilis accusamus natus quos culpa facere illo exercitationem qui!</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+        <Text style={{fontSize:40}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque animi ad optio fugit ratione minus nam dignissimos omnis labore, dolorem nulla quos maiores at ut quaerat expedita molestiae recusandae.</Text>
+      <Text style={{fontSize:50}}>kashan zindabad</Text>
+      </ScrollView>
+    )
+  }
+}
